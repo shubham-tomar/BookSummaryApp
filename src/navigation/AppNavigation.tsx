f24bb4e5944430feb8d0ef1a3c5933f0a0cfaf88
shubham.tomar <shubham.tomar@juspay.in>
2024-12-29 15:53:38 +0530

    ui changes

--- src/navigation/AppNavigation.tsx
+++ src/navigation/AppNavigation.tsx
@@ -1,22 +1,32 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/HomeScreen';
 import BookDetailsScreen from '../screens/BookDetailsScreen';
 import AddSummaryScreen from '../screens/AddSummaryScreen';
 import ProfileScreen from '../screens/ProfileScreen';
+import UploadScreen from '../screens/UploadScreen';
 
 const Stack = createStackNavigator();
+const Tab   = createBottomTabNavigator();
+
+const HomeStack = () => (
+  <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen name="Books" component={HomeScreen} />
+      <Stack.Screen name="BookDetails" component={BookDetailsScreen} />
+      <Stack.Screen name="AddSummary" component={AddSummaryScreen} />
+    </Stack.Navigator>
+);
 
 const AppNavigation: React.FC = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="BookDetails" component={BookDetailsScreen} />
-        <Stack.Screen name="AddSummary" component={AddSummaryScreen} />
-        <Stack.Screen name="Profile" component={ProfileScreen} />
-      </Stack.Navigator>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeStack} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+        <Tab.Screen name="Upload" component={UploadScreen} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 };
---
+++ src/screens/UploadScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const UplaodScreen: React.FC = () => {
+  return (
+    <View>
+      <Text>Upload Document</Text>
+    </View>
+  );
+};
+
+export default UplaodScreen;
